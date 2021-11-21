4f15204dcfe4826da66dd58d8123e5c5990482a0
ubermanu <manu@paca.happy-dev.fr>
2021-11-21 17:45:38 +0100

    get the themes from the vendor dir

--- src/theme.mjs
+++ src/theme.mjs
@@ -2,32 +2,87 @@ import glob from 'fast-glob'
 import { parseString } from 'xml2js'
 import fs from 'fs'
 import less from 'magefront-plugin-less'
+import phpParser from 'php-parser'
 
-// TODO: add support for the themes in the adminhtml
-// TODO: add support for the themes in the vendor directory
+/**
+ * Returns all the themes with their source path.
+ * @return {{path: *, name: string}[]}
+ */
 export const getThemes = () => {
-  const entries = glob.sync('app/design/frontend/*/*/theme.xml')
-  console.log(entries)
+  let themes = {}
+
+  glob.sync('app/design/{frontend,adminhtml}/*/*/theme.xml').forEach((path) => {
+    const name = path.split('/').slice(-3, -1).join('/')
+    let parent = false
+
+    const themeXml = fs.readFileSync(path, 'utf8')
+    parseString(themeXml, (err, res) => {
+      parent = res.theme.parent ? res.theme.parent[0] : false
+    })
+
+    themes[name] = {
+      name,
+      src: path.replace(/\/theme.xml$/, ''),
+      parent
+    }
+  })
+
+  // get the themes from the vendor directory
+  const composerLock = JSON.parse(fs.readFileSync('composer.lock', 'utf8'))
+  const composerThemes = composerLock.packages.filter(
+    (pkg) => pkg.type === 'magento2-theme'
+  )
+
+  composerThemes.forEach((pkg) => {
+    let name = pkg.name
+    const src = `vendor/${pkg.name}`
+    const themeXml = fs.readFileSync(`${src}/theme.xml`, 'utf8')
+    let parent = false
+
+    parseString(themeXml, (err, res) => {
+      parent = res.theme.parent ? res.theme.parent[0] : false
+    })
+
+    const registration = phpParser.tokenGetAll(
+      fs.readFileSync(`${src}/registration.php`, 'utf8')
+    )
+
+    registration.forEach((token) => {
+      if (token[0] === 'T_CONSTANT_ENCAPSED_STRING') {
+        name = token[1]
+          .replace(/^['"]|['"]$/g, '')
+          .replace(/^frontend\/|^adminhtml\//, '')
+      }
+    })
+
+    themes[name] = {
+      name,
+      src,
+      parent
+    }
+  })
+
+  console.log(themes)
+
+  return Object.values(themes)
 }
 
-export const getThemeSrcDir = (name) => {
-  const entries = glob.sync(
-    `app/design/frontend/${name.replace('_', '/')}/theme.xml`
-  )
-  const path = entries.map((entry) => entry.replace('/theme.xml', '')).shift()
-  if (!path) {
+export const getTheme = (name) => {
+  const theme = getThemes().find((theme) => theme.name === name)
+  if (!theme) {
     throw new Error(`Theme ${name} not found`)
   }
-  return path
+  return theme
 }
 
 // TODO: return default config if not defined
 // TODO: get the correct root path
 // TODO: support multiple output languages
 export const getThemeConfig = async (name) => {
-  const src = getThemeSrcDir(name)
+  const theme = getTheme(name)
+
   const customConfig = await import(
-    `${process.cwd()}/${src}/magefront.config.js`
+    `${process.cwd()}/${theme.src}/magefront.config.js`
   ).default
 
   const defaultConfig = {
@@ -35,17 +90,17 @@ export const getThemeConfig = async (name) => {
   }
 
   const config = Object.assign({}, defaultConfig, customConfig)
-  config.src = src
+  config.src = theme.src
   config.dest = `pub/static/frontend/${name.replace('_', '/')}/en_US`
 
   // Get the parent name from the theme.xml file
-  const themeXml = fs.readFileSync(`${src}/theme.xml`, 'utf8')
-  parseString(themeXml, (err, result) => {
-    if (err) {
-      throw new Error(err)
-    }
-    config.parent = result.theme.parent[0] || false
-  })
+  // const themeXml = fs.readFileSync(`${src}/theme.xml`, 'utf8')
+  // parseString(themeXml, (err, result) => {
+  //   if (err) {
+  //     throw new Error(err)
+  //   }
+  //   config.parent = result.theme.parent[0] || false
+  // })
 
   return config
 }
