ca5741692c96c2abab2d904aff7511e4d3b8d27f
ubermanu <manu@paca.happy-dev.fr>
2021-12-04 19:50:55 +0100

    add compat fixes in a preprocessor

--- plugins/less/plugin.mjs
+++ plugins/less/plugin.mjs
@@ -1,12 +1,15 @@
 import gulp from 'gulp'
 import less from 'gulp-less'
 import magentoImport from './lib/magento-import-preprocessor.mjs'
+import less23Compat from './lib/less-23-compat-preprocessor.mjs'
 
 export default (options) => (themeConfig) => {
   // Allow custom options to be passed in via the `options` parameter.
   // Get the modules list from the themeConfig
   // https://github.com/gulp-community/gulp-less#options
-  options = options || { plugins: [magentoImport(themeConfig.modules)] }
+  options = options || {
+    plugins: [magentoImport(themeConfig.modules), less23Compat()]
+  }
 
   return gulp
     .src(`${themeConfig.src}/web/css/*.less`)
