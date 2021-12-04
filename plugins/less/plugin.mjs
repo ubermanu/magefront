0c828ace26cb7ebdd86e760a1e9cc6b0687e7e7d
ubermanu <manu@paca.happy-dev.fr>
2021-12-04 14:27:10 +0100

    clean

--- plugins/less/plugin.mjs
+++ plugins/less/plugin.mjs
@@ -3,11 +3,9 @@ import less from 'gulp-less'
 
 // Generate CSS from LESS source files.
 // Allow custom options to be passed in via the `options` parameter.
-// TODO: Add custom paths to get LESS files from parent theme (to be tested)
-// TODO: Add custom path to get LESS files from node_modules
 export default (options) => (themeConfig) => {
-    return gulp
-        .src(`${themeConfig.src}/web/css/*.less`)
-        .pipe(less(options))
-        .pipe(gulp.dest(`${themeConfig.dest}/css`))
+  return gulp
+    .src(`${themeConfig.src}/web/css/*.less`)
+    .pipe(less(options))
+    .pipe(gulp.dest(`${themeConfig.dest}/css`))
 }
