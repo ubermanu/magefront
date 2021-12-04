6a94037ad83e39c2bac38f1e80e9f31b20a2da95
ubermanu <manu@paca.happy-dev.fr>
2021-12-04 18:40:09 +0100

    allow options override

--- plugins/less/plugin.mjs
+++ plugins/less/plugin.mjs
@@ -2,11 +2,14 @@ import gulp from 'gulp'
 import less from 'gulp-less'
 import magentoImport from './lib/magento-import-preprocessor.mjs'
 
-// TODO: Allow custom options to be passed in via the `options` parameter.
-// TODO: Get the modules list from the themeConfig
 export default (options) => (themeConfig) => {
+  // Allow custom options to be passed in via the `options` parameter.
+  // Get the modules list from the themeConfig
+  // https://github.com/gulp-community/gulp-less#options
+  options = options || { plugins: [magentoImport(themeConfig.modules)] }
+
   return gulp
     .src(`${themeConfig.src}/web/css/*.less`)
-    .pipe(less({ plugins: [magentoImport(themeConfig.modules)] }))
+    .pipe(less(options))
     .pipe(gulp.dest(`${themeConfig.dest}/css`))
 }
