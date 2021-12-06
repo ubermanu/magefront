cd01fb42acde31f13de5c2fd12df6f4cc33a5fa8
ubermanu <manu@paca.happy-dev.fr>
2021-12-06 18:43:09 +0100

    drop gulp support for the less plugin

--- plugins/less/plugin.mjs
+++ plugins/less/plugin.mjs
@@ -1,21 +1,49 @@
 import path from 'path'
-import gulp from 'gulp'
-import less from 'gulp-less'
+import glob from 'fast-glob'
+import fs from 'fs'
+import less from 'less'
 import magentoImport from './lib/magento-import-preprocessor.mjs'
 import less23Compat from './lib/less-23-compat-preprocessor.mjs'
 
+/**
+ * For all the `less` files in the `css` directory, compile them to CSS.
+ * TODO: Add some feedback to the user.
+ *
+ * @param options
+ * @return {(function(*): void)|*}
+ */
 export default (options) => (themeConfig) => {
-  // Allow custom options to be passed in via the `options` parameter.
-  // Get the modules list from the themeConfig
-  // https://github.com/gulp-community/gulp-less#options
-  options = options || {
+  // By default we load these two preprocessors,
+  // so the core Magento 2 themes can be compiled without any configuration.
+  const defaultOptions = {
     plugins: [magentoImport(themeConfig.modules), less23Compat()]
   }
 
+  // Merge the default options with the user-provided options.
+  options = Object.assign({}, defaultOptions, options || {})
   const { src, dest } = options
 
-  return gulp
-    .src(path.join(themeConfig.src, src || 'web/css/!(_)*.less'))
-    .pipe(less(options))
-    .pipe(gulp.dest(path.join(themeConfig.dest, dest || 'css')))
+  // Get the files
+  const files = glob.sync(
+    path.join(themeConfig.src, src || 'web/css/!(_)*.less')
+  )
+
+  for (const file of files) {
+    const destPath = path.join(
+      themeConfig.dest,
+      dest || 'css',
+      path.basename(file, '.less') + '.css'
+    )
+
+    const contents = fs.readFileSync(file, 'utf8')
+    const opts = Object.assign({}, options, { filename: file })
+
+    less.render(contents, opts, (err, output) => {
+      if (err) {
+        throw new Error(err)
+      } else {
+        fs.writeFileSync(destPath, output.css)
+      }
+    })
+  }
 }
