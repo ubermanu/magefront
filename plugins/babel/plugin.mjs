64dd5ff246a60d0d96dbcd9ed7d3ca34c83da9c9
ubermanu <manu@paca.happy-dev.fr>
2021-12-08 00:51:29 +0100

    copy the files to the correct location

--- plugins/babel/plugin.mjs
+++ plugins/babel/plugin.mjs
@@ -1,15 +1,28 @@
 import path from 'path'
 import gulp from 'gulp'
-import babel from 'gulp-babel'
+// import babel from 'gulp-babel'
 
+// TODO: The file is not properly copied when an error occurred
 export default (options) => (themeConfig) => {
   options = options || { presets: ['@babel/env'] }
   const { src, dest } = options
 
-  return gulp
-    .src(path.join(themeConfig.src, src || '**/*/*.js'), {
-      base: themeConfig.src
-    })
-    .pipe(babel(options))
+  // Remove the `web` part from the path
+  // web/**/*.js --> **/*.js
+  // Magento_Catalog/web/**/*.js --> Magento_Catalog/**/*.js
+  const relPath = src || 'web/**/*.js'
+  const relBase = relPath.indexOf('**') !== -1 ? relPath.split('**').shift() : ''
+
+  gulp
+    .src(path.join(themeConfig.src, relPath), { base: path.join(themeConfig.src, relBase), nodir: true })
     .pipe(gulp.dest(path.join(themeConfig.dest, dest || '')))
+
+  themeConfig.modules.forEach((moduleName) => {
+    const modulePath = path.join(themeConfig.src, moduleName)
+    const moduleDest = path.join(themeConfig.dest, moduleName)
+
+    gulp
+      .src(path.join(modulePath, relPath), { base: path.join(modulePath, relBase), nodir: true })
+      .pipe(gulp.dest(path.join(moduleDest, dest || '')))
+  })
 }
