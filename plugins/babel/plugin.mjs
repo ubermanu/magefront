b06b18f9aafcb7776d5175c4ed92ed483133726c
ubermanu <manu@paca.happy-dev.fr>
2021-12-08 01:51:52 +0100

    get the JS files from the web dirs

--- plugins/babel/plugin.mjs
+++ plugins/babel/plugin.mjs
@@ -1,28 +1,23 @@
 import path from 'path'
 import gulp from 'gulp'
-// import babel from 'gulp-babel'
+import babel from 'gulp-babel'
+import { fixMagentoDestWebPaths, getMagentoWebPaths } from 'magefront-plugin-web'
 
-// TODO: The file is not properly copied when an error occurred
 export default (options) => (themeConfig) => {
   options = options || { presets: ['@babel/env'] }
   const { src, dest } = options
 
-  // Remove the `web` part from the path
-  // web/**/*.js --> **/*.js
-  // Magento_Catalog/web/**/*.js --> Magento_Catalog/**/*.js
-  const relPath = src || 'web/**/*.js'
-  const relBase = relPath.indexOf('**') !== -1 ? relPath.split('**').shift() : ''
+  // Get the paths to the public JS files
+  const paths = getMagentoWebPaths(themeConfig, src || '**/*.js')
 
   gulp
-    .src(path.join(themeConfig.src, relPath), { base: path.join(themeConfig.src, relBase), nodir: true })
+    .src(paths, { base: themeConfig.src, nodir: true })
+    .pipe(babel(options))
+    .on('error', (err) => {
+      // Limit the error message length
+      console.error(err.message.slice(0, 2000))
+      this.emit('end')
+    })
+    .pipe(fixMagentoDestWebPaths())
     .pipe(gulp.dest(path.join(themeConfig.dest, dest || '')))
-
-  themeConfig.modules.forEach((moduleName) => {
-    const modulePath = path.join(themeConfig.src, moduleName)
-    const moduleDest = path.join(themeConfig.dest, moduleName)
-
-    gulp
-      .src(path.join(modulePath, relPath), { base: path.join(modulePath, relBase), nodir: true })
-      .pipe(gulp.dest(path.join(moduleDest, dest || '')))
-  })
 }
