c03a99e8d1a3e0425f8d1a1ca4f89fae657c6417
ubermanu <manu@paca.happy-dev.fr>
2022-09-10 13:39:26 +0200

    update main module

--- src/main.mjs
+++ src/main.mjs
@@ -1,2 +1,10 @@
-export { Module, getModules } from './magento/module.mjs'
-export { Theme, getThemes } from './magento/theme.mjs'
+/**
+ * Main module entry point if the package is installed as dependency.
+ * Gives quick access to the magento related functions from the package.
+ */
+
+export * from './env.mjs'
+export * from './magento/composer.mjs'
+export * from './magento/module.mjs'
+export * from './magento/theme.mjs'
+export * from './magento/language.mjs'
