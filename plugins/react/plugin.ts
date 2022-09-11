76b108397ead37d31484f1b8c644cec9db38bf6b
ubermanu <manu@paca.happy-dev.fr>
2022-09-11 19:22:59 +0200

    use ts in the react plugin

--- plugins/react/plugin.ts
+++ plugins/react/plugin.ts
@@ -1,12 +1,16 @@
-import babel from 'magefront-plugin-babel'
+// @ts-ignore
+import babel, { Options as BabelOptions } from 'magefront-plugin-babel'
+
+export interface Options extends BabelOptions {}
 
 /**
  * Transform `*.jsx` files to `*.js` files.
  *
- * @param {{src?:any, ignore?:any, compilerOptions?: {}}} options
+ * @param {Options} options
  * @returns {function(*): Promise<Awaited<unknown>[]>}
  */
-export default (options = {}) => {
+export default (options: Options = {}) => {
+  // @ts-ignore
   const { src, ignore, compilerOptions } = options
 
   const babelReactPreset = {
