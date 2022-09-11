01a0a01799e1e7ac3e8036e3a91e2b3eea361c5c
ubermanu <manu@paca.happy-dev.fr>
2022-09-11 19:11:03 +0200

    use ts in svelte plugin

--- plugins/svelte/plugin.ts
+++ plugins/svelte/plugin.ts
@@ -1,25 +1,33 @@
-import glob from 'fast-glob'
+import glob, { Pattern } from 'fast-glob'
 import path from 'path'
 import fs from 'fs'
 import { compile } from 'svelte/compiler'
+import { CompileOptions } from 'svelte/types/compiler'
+
+export interface Options {
+  src?: string | string[]
+  ignore?: Pattern[]
+  compilerOptions?: CompileOptions
+}
 
 /**
  * Transform `*.svelte` files to `*.js` files.
  *
- * @param {{src?:any, ignore?:any, compilerOptions?: any}} options
+ * @param {Options} options
  * @returns {function(*): Promise<Awaited<*>[]>}
  */
-export default (options = {}) => {
+export default (options: Options = {}) => {
   const { src, ignore, compilerOptions } = options
 
+  // @ts-ignore
   return async (themeConfig) => {
-    const files = await glob(src || '**/*.svelte', { ignore: ignore ?? [], cwd: themeConfig.src })
+    const files = await glob(src ?? '**/*.svelte', { ignore: ignore ?? [], cwd: themeConfig.src })
 
     return Promise.all(
       files.map(async (file) => {
         const filePath = path.join(themeConfig.src, file)
         const fileContent = await fs.promises.readFile(filePath)
-        const output = compile(fileContent.toString(), compilerOptions)
+        const output = compile(fileContent.toString(), compilerOptions ?? {})
 
         return fs.promises.writeFile(filePath.replace(/\.svelte$/, '.js'), output.js.code)
       })
