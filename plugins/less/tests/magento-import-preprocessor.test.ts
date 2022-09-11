71693f6bd8bab554b4e6a59ca13b8579081a0e57
ubermanu <manu@paca.happy-dev.fr>
2022-09-11 22:45:11 +0200

    setup jest instead of uvu

--- plugins/less/tests/magento-import-preprocessor.test.ts
+++ plugins/less/tests/magento-import-preprocessor.test.ts
@@ -1,13 +1,9 @@
-import { test } from 'uvu'
-import * as assert from 'uvu/assert'
 import { preProcessor } from '../lib/magento-import-preprocessor'
 
 test('Replace the `@magento_import` statement with the correct one', () => {
   const pp = new preProcessor(['Magento_Catalog'])
-  assert.is(
-    pp.process("//@magento_import 'source/_module.less';"),
+
+  expect(pp.process("//@magento_import 'source/_module.less';")).toEqual(
     "@import (optional) '../../Magento_Catalog/web/css/source/_module.less';"
   )
 })
-
-test.run()
