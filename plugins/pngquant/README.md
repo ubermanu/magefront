1f937ea4ae7518a7efdb47da970ad77a3a880873
ubermanu <manu@paca.happy-dev.fr>
2022-09-19 21:07:28 +0200

    add pngquant plugin (wip)

---
+++ plugins/pngquant/README.md
@@ -0,0 +1,21 @@
+# magefront-plugin-pngquant
+
+Optimize PNG files with [pngquant](https://pngquant.org/).
+
+## Install
+
+    npm i magefront-plugin-pngquant
+
+## Usage
+
+```js
+import pngquant from 'magefront-plugin-pngquant'
+
+export default {
+    plugins: [
+        pngquant()
+    ]
+}
+```
+
+See the [documentation](https://ubermanu.github.io/magefront/#/plugins/pngquant) for more information.
---
+++ plugins/pngquant/plugin.ts
@@ -0,0 +1,40 @@
+// @ts-nocheck
+import glob, { Pattern } from 'fast-glob'
+import path from 'path'
+import fs from 'fs'
+import pngquant from 'pngquant'
+
+export interface Options {
+  src?: string | string[]
+  ignore?: Pattern[]
+  args?: Array<string | number>
+}
+
+/**
+ * Optimize PNG files.
+ *
+ * @param {Options} options
+ * @returns {function(*): Promise<Awaited<*>[]>}
+ */
+export default (options: Options = {}) => {
+  const { src, ignore, args } = options
+
+  // @ts-ignore
+  return async (buildContext) => {
+    const files = await glob(src ?? '**/*.png', { ignore, cwd: buildContext.src })
+
+    await Promise.all(
+      files.map(async (file) => {
+        const filePath = path.join(buildContext.src, file)
+        const fileContent = await fs.promises.readFile(filePath)
+        const result = await pngquant(args)(fileContent)
+
+        if (result) {
+          return fs.promises.writeFile(filePath, result)
+        } else {
+          console.error('PNGQUANT error', result)
+        }
+      })
+    )
+  }
+}
