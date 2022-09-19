9a4f6b171f187051e3e6ea4519dbc5569edac7a1
ubermanu <manu@paca.happy-dev.fr>
2022-09-19 19:39:45 +0200

    add svgo plugin

---
+++ plugins/svgo/plugin.ts
@@ -0,0 +1,39 @@
+import glob, { Pattern } from 'fast-glob'
+import path from 'path'
+import fs from 'fs'
+import svgo, { OptimizeOptions } from 'svgo'
+
+export interface Options {
+  src?: string | string[]
+  ignore?: Pattern[]
+  optimizeOptions?: OptimizeOptions
+}
+
+/**
+ * Transform Stylus files to CSS.
+ *
+ * @param {Options} options
+ * @returns {function(*): Promise<Awaited<*>[]>}
+ */
+export default (options: Options = {}) => {
+  const { src, ignore, optimizeOptions } = options
+
+  // @ts-ignore
+  return async (buildContext) => {
+    const files = await glob(src ?? '**/*.svg', { ignore, cwd: buildContext.src })
+
+    await Promise.all(
+      files.map(async (file) => {
+        const filePath = path.join(buildContext.src, file)
+        const fileContent = await fs.promises.readFile(filePath)
+        const result = await svgo.optimize(fileContent.toString(), optimizeOptions)
+
+        if ('data' in result) {
+          await fs.promises.writeFile(filePath, result.data)
+        } else {
+          console.error('SVGO error', result)
+        }
+      })
+    )
+  }
+}
