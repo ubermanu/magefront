47d1f91eada6660ba892a4608085c72986f8e419
ubermanu <manu@paca.happy-dev.fr>
2022-09-20 13:01:19 +0200

    add concat plugin

---
+++ plugins/concat/README.md
@@ -0,0 +1,21 @@
+# magefront-plugin-concat
+
+Merge files into one.
+
+## Install
+
+    npm i magefront-plugin-concat
+
+## Usage
+
+```js
+import concat from 'magefront-plugin-concat'
+
+export default {
+    plugins: [
+        concat({ src: '**/*.css', dest: 'css/all-styles.css' }),
+    ]
+}
+```
+
+See the [documentation](https://ubermanu.github.io/magefront/#/plugins/concat) for more information.
---
+++ plugins/concat/plugin.ts
@@ -0,0 +1,53 @@
+import glob, { Pattern } from 'fast-glob'
+import path from 'path'
+import fs from 'fs'
+
+export interface Options {
+  src: string | string[]
+  ignore?: Pattern[]
+  dest: string
+  remove?: boolean
+}
+
+/**
+ * Merge files into one.
+ *
+ * @param {Options} options
+ * @returns {(function(*): Promise<void>)|*}
+ */
+export default (options: Options) => {
+  const { src, ignore, dest, remove } = options
+
+  // @ts-ignore
+  return async (buildContext) => {
+    if (!src) {
+      throw new Error('Missing "src" option')
+    }
+
+    if (!dest) {
+      throw new Error('Missing "dest" option')
+    }
+
+    const files = await glob(src, { ignore, cwd: buildContext.src })
+
+    const packed = await Promise.all(
+      files.map((file: string) => {
+        const filePath = path.join(buildContext.src, file)
+        return fs.promises.readFile(filePath, 'utf8')
+      })
+    )
+
+    // Write the merged file
+    await fs.promises.writeFile(path.join(buildContext.src, dest), packed.join('\n'), 'utf8')
+
+    // Delete the original files
+    if (remove ?? true) {
+      await Promise.all(
+        files.map((file: string) => {
+          const filePath = path.join(buildContext.src, file)
+          return fs.promises.unlink(filePath)
+        })
+      )
+    }
+  }
+}
