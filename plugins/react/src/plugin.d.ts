43e45a09bc22cd013768894500438917977651ca
ubermanu <manu@paca.happy-dev.fr>
2022-10-05 13:45:38 +0200

    fix react build

---
+++ plugins/react/src/plugin.d.ts
@@ -0,0 +1,4 @@
+import type { Options as BabelOptions } from 'magefront-plugin-babel'
+
+// FIXME: This is a workaround for generating types for the `react` plugin.
+export interface Options extends BabelOptions {}
