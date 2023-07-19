cbc0f90fa2541d4ef56f3a5c812ef6ec30aae4f0
ubermanu <manu@paca.happy-dev.fr>
2023-07-19 18:34:22 +0200

    feat: update sass and drop node-sass support

--- plugins/sass/src/plugin.ts
+++ plugins/sass/src/plugin.ts
@@ -1,21 +1,23 @@
 import glob, { type Pattern } from 'fast-glob'
 import type { Plugin } from 'magefront'
-import fs from 'node:fs'
+import fs from 'node:fs/promises'
 import path from 'node:path'
-import dartSass, { type Options as RenderOptions, type SassException, type Result as SassResult } from 'sass'
+import { compileAsync, type Options as SassOptions } from 'sass'
 
 export interface Options {
   src?: string | string[]
   ignore?: Pattern[]
   sourcemaps?: boolean
-  compiler?: any
-  compilerOptions?: RenderOptions
+  compilerOptions?: SassOptions<'async'>
 }
 
-/** Compile SCSS files to CSS. You can use the `compiler` option to specify the sass compiler to use. (e.g. node-sass) */
+/**
+ * Compile SCSS files to CSS.
+ *
+ * Note: This plugin is not compatible with node-sass because it has been deprecated.
+ */
 export default (options: Options = {}): Plugin => {
-  const { src, ignore, sourcemaps, compiler, compilerOptions } = { ...options }
-  const sass = compiler ?? dartSass
+  const { src, ignore, sourcemaps, compilerOptions } = { ...options }
 
   return async (context) => {
     const files = await glob(src ?? '**/!(_)*.scss', {
@@ -26,26 +28,15 @@ export default (options: Options = {}): Plugin => {
     await Promise.all(
       files.map(async (file) => {
         const filePath = path.join(context.src, file)
+        const output = await compileAsync(filePath, compilerOptions)
 
-        return sass.render(
-          {
-            ...compilerOptions,
-            file: filePath,
-          },
-          (err: SassException, output: SassResult) => {
-            if (err) {
-              console.error(err)
-            } else {
-              const cssFilePath = filePath.replace(/\.scss$/, '.css')
-              fs.writeFileSync(cssFilePath, output.css, 'utf8')
+        const cssFilePath = filePath.replace(/\.scss$/, '.css')
+        await fs.writeFile(cssFilePath, output.css, 'utf8')
 
-              // TODO: The map is always null?
-              if (sourcemaps && output.map) {
-                fs.writeFileSync(`${cssFilePath}.map`, JSON.stringify(output.map), 'utf8')
-              }
-            }
-          }
-        )
+        // TODO: The map is always null?
+        if (sourcemaps && output.sourceMap) {
+          await fs.writeFile(`${cssFilePath}.map`, JSON.stringify(output.sourceMap), 'utf8')
+        }
       })
     )
   }
