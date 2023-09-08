053b6cee0c70ce76f7445abb322667080ff46df7
Manu <e.vodor@gmail.com>
2023-09-08 20:07:18 +0200

    feat: increase code coverage (#69)
    * feat: less plugin tests
    
    * feat: js-translation test
    
    * feat: requirejs-config tests

---
+++ packages/plugin-less/tests/plugin.test.ts
@@ -0,0 +1,40 @@
+import magefront from 'magefront'
+import fs from 'node:fs'
+import path from 'node:path'
+import process from 'node:process'
+import less from '../src/plugin'
+
+const rootPath = process.env.MAGEFRONT_TEST_MAGENTO_ROOT!
+
+test('Parses less files without any options', async () => {
+  await magefront({
+    theme: 'Magento/blank',
+    plugins: [less()],
+    magento: { rootPath },
+  })
+
+  expect(fs.existsSync(path.join(rootPath, 'pub/static/frontend/Magento/blank/en_US/css/styles-m.css'))).toBe(true)
+  expect(fs.existsSync(path.join(rootPath, 'pub/static/frontend/Magento/blank/en_US/css/styles-l.css'))).toBe(true)
+  expect(fs.existsSync(path.join(rootPath, 'pub/static/frontend/Magento/blank/en_US/css/email.css'))).toBe(true)
+  expect(fs.existsSync(path.join(rootPath, 'pub/static/frontend/Magento/blank/en_US/css/email-inline.css'))).toBe(true)
+  expect(fs.existsSync(path.join(rootPath, 'pub/static/frontend/Magento/blank/en_US/css/print.css'))).toBe(true)
+}, 5000)
+
+test('Parses less files with sourcemaps', async () => {
+  await magefront({
+    theme: 'Magento/blank',
+    plugins: [less({ sourcemaps: true })],
+    magento: { rootPath },
+  })
+
+  expect(fs.existsSync(path.join(rootPath, 'pub/static/frontend/Magento/blank/en_US/css/styles-m.css'))).toBe(true)
+  expect(fs.existsSync(path.join(rootPath, 'pub/static/frontend/Magento/blank/en_US/css/styles-m.css.map'))).toBe(true)
+  expect(fs.existsSync(path.join(rootPath, 'pub/static/frontend/Magento/blank/en_US/css/styles-l.css'))).toBe(true)
+  expect(fs.existsSync(path.join(rootPath, 'pub/static/frontend/Magento/blank/en_US/css/styles-l.css.map'))).toBe(true)
+  expect(fs.existsSync(path.join(rootPath, 'pub/static/frontend/Magento/blank/en_US/css/email.css'))).toBe(true)
+  expect(fs.existsSync(path.join(rootPath, 'pub/static/frontend/Magento/blank/en_US/css/email.css.map'))).toBe(true)
+  expect(fs.existsSync(path.join(rootPath, 'pub/static/frontend/Magento/blank/en_US/css/email-inline.css'))).toBe(true)
+  expect(fs.existsSync(path.join(rootPath, 'pub/static/frontend/Magento/blank/en_US/css/email-inline.css.map'))).toBe(true)
+  expect(fs.existsSync(path.join(rootPath, 'pub/static/frontend/Magento/blank/en_US/css/print.css'))).toBe(true)
+  expect(fs.existsSync(path.join(rootPath, 'pub/static/frontend/Magento/blank/en_US/css/print.css.map'))).toBe(true)
+}, 5000)
---
+++ packages/plugin-requirejs-config/tests/plugin.test.ts
@@ -0,0 +1,21 @@
+import magefront from 'magefront'
+import fs from 'node:fs'
+import path from 'node:path'
+import process from 'node:process'
+import requireJsConfig from '../src/plugin'
+
+const rootPath = process.env.MAGEFRONT_TEST_MAGENTO_ROOT!
+
+test('Generate js-translation.json', async () => {
+  await magefront({
+    theme: 'Magento/blank',
+    plugins: [requireJsConfig()],
+    magento: { rootPath },
+  })
+
+  expect(fs.existsSync(path.join(rootPath, 'pub/static/frontend/Magento/blank/en_US/requirejs-config.js'))).toBe(true)
+
+  const content = await fs.promises.readFile(path.join(rootPath, 'pub/static/frontend/Magento/blank/en_US/requirejs-config.js'), 'utf8')
+  expect(content).toContain('var config = {')
+  expect(content).toContain('require.config(config);')
+}, 5000)
