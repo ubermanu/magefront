ce7555643a64e2788d65cab7067089c966fee188
ubermanu <manu@paca.happy-dev.fr>
2023-09-12 10:02:09 +0200

    fix: preserve import options with magento_import

--- packages/plugin-less/tests/magento-import-preprocessor.test.ts
+++ packages/plugin-less/tests/magento-import-preprocessor.test.ts
@@ -5,3 +5,19 @@ test('Replace the `@magento_import` statement with the correct one', () => {
 
   expect(pp.process("//@magento_import 'source/_module.less';")).toEqual("@import (optional) '../Magento_Catalog/css/source/_module.less';")
 })
+
+test('Preserves import parameters', () => {
+  const pp = new preProcessor(['Magento_Catalog'])
+
+  expect(pp.process("//@magento_import (reference) 'source/_module.less';")).toEqual(
+    "@import (optional, reference) '../Magento_Catalog/css/source/_module.less';"
+  )
+
+  expect(pp.process("//@magento_import (optional) 'source/_module.less';")).toEqual(
+    "@import (optional) '../Magento_Catalog/css/source/_module.less';"
+  )
+
+  expect(pp.process("//@magento_import (multiple, less, inline) 'source/_module.less';")).toEqual(
+    "@import (optional, multiple, less, inline) '../Magento_Catalog/css/source/_module.less';"
+  )
+})
