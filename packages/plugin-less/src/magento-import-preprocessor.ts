5013afacc4f294031a2fbfdfee67f5066429f500
Manu <e.vodor@gmail.com>
2023-09-12 10:07:33 +0200

    fix: @magent_import plugin not preserving parameters (#81)
    * importe less types in tsconfig
    
    * fix: preserve import options with magento_import
    
    * chore: changeset

--- packages/plugin-less/src/magento-import-preprocessor.ts
+++ packages/plugin-less/src/magento-import-preprocessor.ts
@@ -1,6 +1,3 @@
-// Hacky way to import less type definitions
-import 'less'
-
 /** Replaces the commented `@magento_import` statements with the actual import statements for each enabled modules. */
 export class preProcessor implements Less.PreProcessor {
   private modules: string[]
@@ -10,9 +7,22 @@ export class preProcessor implements Less.PreProcessor {
   }
 
   process(src: string): string {
-    return src.replace(/^\/\/@magento_import(.*);(.*)$/gm, (match, path) => {
+    return src.replace(/^\/\/@magento_import\s(\([\w\s,]*\))?(.*)\s?;.*$/gm, (match, options, path) => {
+      const params: string[] = []
+
+      if (options) {
+        options = options.trim().replace(/^\(/, '').replace(/\)$/, '')
+        options = options.split(',').map((p: string) => p.trim())
+        params.push(...options)
+      }
+
+      if (!params.includes('optional')) {
+        params.unshift('optional')
+      }
+
       path = path.replace(/["']/g, '').trim()
-      return this.modules.map((mod) => `@import (optional) '../${mod}/css/${path}';`).join('\n')
+
+      return this.modules.map((mod) => `@import (${params.join(', ')}) '../${mod}/css/${path}';`).join('\n')
     })
   }
 }
