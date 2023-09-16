111e7ef717198f0d5cfa427360101b6668e7cc1d
ubermanu <manu@paca.happy-dev.fr>
2023-09-16 20:00:21 +0200

    fix imagemin impl

--- packages/plugin-imagemin/src/plugin.ts
+++ packages/plugin-imagemin/src/plugin.ts
@@ -2,6 +2,7 @@ import glob, { type Pattern } from 'fast-glob'
 import type { Plugin as ImageMinPlugin } from 'imagemin'
 import imagemin from 'imagemin'
 import type { Plugin } from 'magefront'
+import fs from 'node:fs/promises'
 import path from 'node:path'
 
 export interface Options {
@@ -21,9 +22,25 @@ export default (options?: Options): Plugin => {
       ignore,
     })
 
-    await imagemin(files, {
-      destination: path.join(context.src, dest ?? ''),
-      plugins,
-    })
+    const result = await imagemin(
+      files.map((file) => path.join(context.src, file)),
+      {
+        plugins,
+      }
+    )
+
+    context.logger.info(`Optimized ${result.length} images.`)
+
+    // For each optimized image, write it to the destination.
+    await Promise.all(
+      result.map(async (file) => {
+        // Remove the absolute context.src path from the file's source path.
+        const sourcePath = path.relative(context.src, file.sourcePath)
+        const filePath = path.join(context.src, dest ?? '', sourcePath)
+
+        // Write the optimized image to the destination.
+        await fs.writeFile(filePath, file.data, 'utf8')
+      })
+    )
   }
 }
