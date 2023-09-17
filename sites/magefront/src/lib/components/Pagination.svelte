3afb7f0a6810475ade6892ac425d9bc59c9ddf7d
ubermanu <manu@paca.happy-dev.fr>
2023-09-17 21:02:43 +0200

    update layout

--- sites/magefront/src/lib/components/Pagination.svelte
+++ sites/magefront/src/lib/components/Pagination.svelte
@@ -6,7 +6,7 @@
   $: next = $page.data.pagination?.next
 </script>
 
-<div class="container mx-auto flex items-center justify-between text-neutral-300">
+<div class="container mx-auto flex max-w-3xl items-center justify-between text-neutral-300">
   {#if previous}
     <a class="mr-auto flex items-center p-2 pl-0" href="/docs/{previous.slug}" rel="prev">
       <ChevronLeft class="mr-2 h-6 w-6" />
--- sites/magefront/src/routes/docs/+layout.svelte
+++ sites/magefront/src/routes/docs/+layout.svelte
@@ -2,10 +2,10 @@
   import Pagination from '$lib/components/Pagination.svelte'
 </script>
 
-<div class="prose prose-neutral prose-invert max-w-none flex-grow py-10">
-  <div class="container mx-auto">
-    <slot />
-  </div>
+<div class="prose prose-neutral prose-invert container mx-auto max-w-3xl flex-grow py-10">
+  <slot />
 </div>
+
+<hr class="container mx-auto my-5 max-w-3xl border-neutral-600 opacity-10" />
 
 <Pagination />
