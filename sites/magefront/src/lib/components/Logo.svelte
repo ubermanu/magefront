5fdb82168da81345c00cfa8ba684a1c1cceaefe9
ubermanu <manu@paca.happy-dev.fr>
2023-09-18 14:24:21 +0200

    add missing base in logo url

--- sites/magefront/src/lib/components/Logo.svelte
+++ sites/magefront/src/lib/components/Logo.svelte
@@ -5,7 +5,7 @@
   const { class: additionalClass } = $$restProps
 </script>
 
-<a class="logo flex items-center {additionalClass}" href="/">
+<a class="logo flex items-center {additionalClass}" href="{base}/">
   <img src="{base}/magefront-logo.svg" alt="" class="mr-2 h-9 w-9" />
   <p class="font-serif text-2xl font-light">Magefront</p>
   <div class="ml-3 rounded bg-neutral-600 px-1 py-0.5 text-center text-xs text-white shadow">
