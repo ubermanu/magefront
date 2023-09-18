0078fd227fa54c1d91f7f6c6f6569774b667f5f4
ubermanu <manu@paca.happy-dev.fr>
2023-09-18 14:25:44 +0200

    minor change

--- sites/magefront/src/routes/docs/+page.js
+++ sites/magefront/src/routes/docs/+page.js
@@ -2,6 +2,6 @@ import { base } from '$app/paths'
 import { redirect } from '@sveltejs/kit'
 
 /** @type {import('./$types').PageLoad} */
-export const load = async ({ url }) => {
+export const load = async () => {
   throw redirect(301, `${base}/docs/getting-started`)
 }
