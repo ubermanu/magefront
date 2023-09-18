472d943754aedcb4763faa5f982ba7dd39362437
ubermanu <manu@paca.happy-dev.fr>
2023-09-18 12:59:27 +0200

    break footer text more gracefully

--- sites/magefront/src/routes/+layout.svelte
+++ sites/magefront/src/routes/+layout.svelte
@@ -22,7 +22,11 @@
 
     <footer class="py-8">
       <div class="container mx-auto text-center">
-        <p class="text-sm text-neutral-400">&copy; {new Date().getFullYear()} Magefront and its contributors. Licensed under MIT.</p>
+        <p class="text-sm text-neutral-400">
+          &copy; {new Date().getFullYear()} Magefront and its contributors.
+          <br class="sm:hidden" />
+          Licensed under MIT.
+        </p>
       </div>
     </footer>
   </main>
