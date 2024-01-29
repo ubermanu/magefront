73c764667a727ba48fee54d70e575097edb80aee
ubermanu <manu@paca.happy-dev.fr>
2024-01-29 17:02:27 +0100

    update fz and spacing on home page

--- sites/magefront/src/routes/+page.svelte
+++ sites/magefront/src/routes/+page.svelte
@@ -19,10 +19,12 @@
   </svelte:fragment>
 </Header>
 
-<section class="flex flex-grow items-center py-20">
+<section class="flex flex-grow items-center py-10 md:py-20">
   <div class="container mx-auto max-w-4xl text-center">
-    <h1 class="mb-4 text-6xl font-black">Develop themes using a stack you already know.</h1>
-    <p class="mx-auto mb-4 max-w-3xl text-xl leading-relaxed text-neutral-400">
+    <h1 class="mb-8 text-4xl font-black leading-tight md:text-5xl xl:text-6xl">
+      Develop themes using a stack you already know.
+    </h1>
+    <p class="mx-auto mb-4 max-w-3xl text-lg md:text-xl leading-relaxed text-neutral-400">
       A CLI tool for building Magento 2 themes with any modern frontend stack, including <span class="text-red-600">SASS</span>,
       <span class="text-red-600">Tailwind</span>, <span class="text-red-600">Svelte</span>, <span class="text-red-600">TypeScript</span>,
       and more.
